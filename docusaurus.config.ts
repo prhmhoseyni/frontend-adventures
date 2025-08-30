4e960892f59c7f003de7e0a8934c125e28329e02
PRHM <prhmhoseyni@gmail.com>
2025-08-30 17:31:35 +0330

    Edit primary color

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Frontend Adventures',
+  tagline: 'Welcome to frontend adventures!',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -73,9 +73,9 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'Frontend Adventures',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'Frontend Adventures Logo',
         src: 'img/logo.svg',
       },
       items: [
